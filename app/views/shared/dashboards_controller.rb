79d27bebf6201667626580357412994c3ab15ebd
leepuppychow <leepuppychow@gmail.com>
2018-02-06 15:13:42 -0700

    remove extraneous files

--- app/views/shared/dashboards_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::DashboardsController < Admin::BaseController
-  def index
-    @categories= Category.all
-    @organization = Organization.new
-    @needs = Organization.find_by(user_id: current_user.id).needs.all if Organization.find_by(user_id: current_user)
-    @need = Organization.find_by(user_id: current_user.id).needs.new if Organization.find_by(user_id: current_user)
-  end
-
-end
