31bd94f834221369f1974cb4e5af211612870261
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-06 12:40:56 -0700

    added total_price column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918203915) do
+ActiveRecord::Schema.define(version: 20180206184641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170918203915) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.float "total_price"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
