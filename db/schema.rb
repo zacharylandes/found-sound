9f586db8e3551ec5fbf6013f50ed01125be117f5
maxscores <maxstackhouse@gmail.com>
2018-02-07 13:27:05 -0700

    Add Slug Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207143719) do
+ActiveRecord::Schema.define(version: 20180207200835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20180207143719) do
     t.string "name"
     t.string "address"
     t.string "status"
+    t.string "slug"
   end
 
   create_table "users", force: :cascade do |t|
