c6e9ad3269333b3dcf42937617ac86f4c1128553
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-07 12:04:02 -0700

    update category routes

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 RSpec.describe "As a visitor can visit category show page" do
     it " can visit category show page" do
         create_items
-        
-        visit '/cats'
-        
+
+        visit '/categories/cats'
+
     end
 end
