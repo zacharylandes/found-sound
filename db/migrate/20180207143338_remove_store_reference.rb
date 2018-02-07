58bc788a309b1b4d76e44ece74a851b9a1a43d90
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-07 07:52:57 -0700

    migrating for travis

--- db/migrate/20180207143338_remove_store_reference.rb
+++
@@ -1,6 +0,0 @@
-class RemoveStoreReference < ActiveRecord::Migration[5.1]
-  def change
-    remove_reference :items, :store, index: true, foreign_key: true
-
-  end
-end
--- db/migrate/20180207143509_remove_stores.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStores < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :stores
-  end
-end
