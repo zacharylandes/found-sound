0dda7f96229d33012f013ce8d7177e94d16fc3b6
maxscores <maxstackhouse@gmail.com>
2018-02-07 13:27:52 -0700

    Add set_slug callback and active_items method

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,13 @@
 class Store < ApplicationRecord
   has_many :items
   validates_presence_of :name, :address, :status
+  before_save :set_slug
+
+  def set_slug
+    self.slug = name.parameterize
+  end
+
+  def active_items
+    items.where(condition: 'active')
+  end
 end
