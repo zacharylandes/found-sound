4af92cb52e2c7029b08092516ed66e773cb18e49
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-07 16:16:55 -0700

    merging master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207200835) do
+ActiveRecord::Schema.define(version: 20180207210731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,13 +65,10 @@ ActiveRecord::Schema.define(version: 20180207200835) do
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.string "address"
-
-    t.string "slug"
     t.string "status", default: "pending"
-
     t.bigint "user_id"
+    t.string "slug"
     t.index ["user_id"], name: "index_stores_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
