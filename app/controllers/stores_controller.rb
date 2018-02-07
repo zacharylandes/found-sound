ea78043239ba0e760dab2f1cc98f10a6a2e2ba91
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-07 13:10:29 -0700

    passing store creation tests

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,28 @@
 class StoresController < ApplicationController
-  def show
-    @store = Store.find_by(slug: params[:store])
+
+  def index
+    @stores = current_user.stores
   end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    store = Store.new(store_params)
+    if store.save!
+      redirect_to dashboard_index_path
+    else
+      flash[:notice] = "Invalid Credentials"
+      render :new
+    end
+  end
+
+private
+
+  def store_params
+    params[:store][:user_id] = current_user.id
+    params.require(:store).permit(:name, :address, :user_id, :status)
+  end
+
 end
