6411c53bb3885a5087e5c5b1d1d411ecf3bc6070
maxscores <maxstackhouse@gmail.com>
2018-02-08 13:57:59 -0700

    fix user visit category show test

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 RSpec.describe "As a visitor can visit category show page" do
-    it " can visit category show page" do
-        create_items
+  it " can visit category show page" do
+    cats = create(:category, title: "Cats")
+    create_list(:item, 2, category: cats)
 
-        visit '/categories/cats'
-
-    end
+    visit '/categories/cats'
+  end
 end
