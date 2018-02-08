5cc96ad21d5ba8938ea1e313568eff9469ccf523
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-07 17:46:28 -0700

    Rearranged faulty test setup making it clearer

--- spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_index_spec.rb
+++ spec/features/user/items/user_cant_see_add_to_cart_for_retired_item_in_index_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'As a visitor' do
 		it 'a user cannot see add to cart for a retired item' do
 			category = Category.create(title: "Animals")
 			one_url = "http://pandathings.com/wp-content/uploads/2016/10/onesie-6-300x300.png"
-			item_one = category.items.create(title: "Funsie Onesie", description: "number one", price: 8.00,
-			image: one_url, condition: 'retired' )
-			
-			visit items_path
-			
+			item_one = create(:item, condition: 'retired')
+			category.items << item_one
+
+			visit store_path(item_one.store.slug)
+
 			expect(page).not_to have_content("Add to cart")
 		end
 	end
