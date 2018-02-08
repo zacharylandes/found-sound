f2ff07772562070d98f251dbf4868d3fdfb6c9bd
maxscores <maxstackhouse@gmail.com>
2018-02-08 13:58:16 -0700

    fix unauthentic user view other user data test

--- spec/features/user/authentication/unauthentic_user_cannot_view_another_users_data_spec.rb
+++ spec/features/user/authentication/unauthentic_user_cannot_view_another_users_data_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "Unauthenticated users security" do
-  before(:each) do
-    setup
-  end
+  let(:user_1) {create(:user, first_name: "Mimi", last_name: "Le", email: "mimi@mimi.com", password: "mimi")}
+  let(:user_2) {create(:user, first_name: "Jake", last_name: "the Dog", email: "jake@adventuretime.com", password: "dog")}
+  let(:order) {create(:order, status: "ordered", user: user_1)}
+
   context "As an unauthenticated user" do
     it "I cannot view another user’s private data" do
       visit dashboard_index_path
 
       expect(current_path).to eq(login_path)
 
-      visit order_path(@order)
+      visit order_path(order)
 
       expect(current_path).to eq(login_path)
     end
 
     it "I should be redirected to login/create account when I try to check out" do
-      visit item_path(@unicorn_onesie_1)
+      unicorn_onesie_1 = create(:item)
+
+      visit item_path(unicorn_onesie_1)
 
       click_on "Add to cart"
 
