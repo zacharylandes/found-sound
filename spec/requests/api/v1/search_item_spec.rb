b3b3f00b083949caba0d4fc6e17f25858c88642f
maxscores <maxstackhouse@gmail.com>
2018-02-10 13:43:50 -0700

    Update Test to test for exact response

--- spec/requests/api/v1/search_item_spec.rb
+++ spec/requests/api/v1/search_item_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 describe "As a user with an API key" do
   describe "when I send a get request to the search api endpoint" do
     it "returns a json response with items with matching name and description" do
@@ -23,18 +24,24 @@ describe "As a user with an API key" do
 
       search_response = JSON.parse(response.body)
 
-      expect(search_response["type"]).to eq('items')
-      expect(search_response["q"]).to eq("diapers")
-      expect(search_response["results"].count).to eq(2)
-      expect(search_response["results"].first["title"]).to eq(item_1.title)
-      expect(search_response["results"].first["description"]).to eq(item_1.description)
-      expect(search_response["results"].first["price"]).to eq("$30.00")
-      expect(search_response["results"].first["id"]).to eq(item_1.id)
-      expect(search_response["results"].first["image"]).to be_nil
-      expect(search_response["results"].first["condition"]).to be_nil
-      expect(search_response["results"].first["store_id"]).to be_nil
-      
-      expect(search_response["results"].last["title"]).to eq(item_2.title)
+      expected_item_search_response = {
+        "type"=> "items",
+        "q"=> "diapers",
+        "results"=> [
+            {"id"=> item_1.id,"title"=> "Nature's Diapers",
+              "description"=> "No chemicals or dyes. Also, they don't really work.",
+              "price"=> "$30.00"
+            },
+            {
+              "id"=> item_2.id,
+              "title"=> "Dopo Tesigns Shoulder Bag",
+              "description"=> "Versitile bag great for commuting. Even makes as an excellent diaper bag. #dadcore #momcore",
+              "price"=> "$125.00"
+            }
+          ]
+        }
+
+      expect(search_response).to eq(expected_item_search_response)
     end
 
     it "returns invalid request when a bad api key is sent" do
