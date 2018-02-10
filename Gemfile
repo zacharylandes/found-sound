33769d6a4843eca85a353567af17678daef1dfe7
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-09 19:30:09 -0700

    showing a stores items

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem "paperclip", "~> 5.0.0"
 gem 'chart-js-rails'
 gem 'cloudinary'
 gem 'figaro'
+gem 'will_paginate', '~> 3.1.0'
 gem 'carrierwave'
 gem 'active_model_serializers', '~> 0.10.0'
 
--- spec/features/store_manager/store_manager_can_manage_items_spec.rb
+++ spec/features/store_manager/store_manager_can_manage_items_spec.rb
@@ -19,6 +19,22 @@ describe "As a store manager" do
         expect(page).to have_content(item2.title)
         expect(page).to have_content(item3.title)
       end
+      it "can click on My Items and see all items in my store" do
+        store_manager = create(:user)
+        store = create(:store)
+        user_store = create(:user_store, user: store_manager, store: store, user_type: 0)
+        item1, item2, item3 = create_list(:item, 3, store: store)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(store_manager)
+
+        visit dashboard_index_path
+        click_on "My Stores"
+        click_on "#{store.name}"
+
+        expect(current_path).to eq "/#{store.slug}"
+        expect(page).to have_content(item1.title)
+        expect(page).to have_content(item2.title)
+        expect(page).to have_content(item3.title)
+      end
     end
   end
 end
@@ -33,14 +49,15 @@ describe "As an unregistered user" do
       item1, item2, item3 = create_list(:item, 3, store: store)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(random_user)
 
-      visit dashboard_index_path
-      expect(page).to have_content("Not Found")
+
+      visit admin_dashboard_index_path
+       expect{admin_dashboard_index_path}.to  raise_error(ActiveRecord::RecordNotFound)
 
       visit orders_path
-      expect(page).to have_content("Not Found")
+      expect(page.status_code).to eq(404)
 
       visit stores_path
-      expect(page).to have_content("Not Found")
+      expect(page.status_code).to eq(404)
     end
   end
 end
