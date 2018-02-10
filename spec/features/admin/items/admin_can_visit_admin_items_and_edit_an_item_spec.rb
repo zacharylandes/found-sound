4c10a1587b8bddcfeb4bda7b9a68a58891c56520
maxscores <maxstackhouse@gmail.com>
2018-02-09 18:29:17 -0700

    Add VCR to item image upload

--- spec/features/admin/items/admin_can_visit_admin_items_and_edit_an_item_spec.rb
+++ spec/features/admin/items/admin_can_visit_admin_items_and_edit_an_item_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe 'an admin can visit admin dashboard' do
       fill_in "item[title]", with: "White Cat Twosie"
       fill_in "item[description]", with: "two is better"
       fill_in "item[price]", with: "39.99"
-      page.attach_file("item[image]", testing_image)
       click_on "Upload Item"
 
       expect(page).to have_content("White Cat Twosie")
