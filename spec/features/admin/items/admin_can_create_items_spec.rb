968453b9db5872b0ad7d708f9a689aaacf6d5143
maxscores <maxstackhouse@gmail.com>
2018-02-09 18:29:17 -0700

    Add VCR to item image upload

--- spec/features/admin/items/admin_can_create_items_spec.rb
+++ spec/features/admin/items/admin_can_create_items_spec.rb
@@ -3,22 +3,24 @@ require 'rails_helper'
 RSpec.feature "Admin item creation" do
   context "As an authenticated admin" do
     it "I can create an item" do
-      admin = build(:admin)
-      create(:store)
-      create(:item)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      VCR.use_cassette("admin/admin_can_create_items") do
+        admin = build(:admin)
+        create(:store)
+        create(:item)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit admin_items_path
-      click_on "Create New Item"
-      fill_in "item[title]", with: "Onesie"
-      fill_in "item[description]", with: "This Onesie is awesome!"
-      fill_in "item[price]", with: "59.99"
-      page.attach_file("item[image]", testing_image)
-      click_on "Upload Item"
+        visit admin_items_path
+        click_on "Create New Item"
+        fill_in "item[title]", with: "Onesie"
+        fill_in "item[description]", with: "This Onesie is awesome!"
+        fill_in "item[price]", with: "59.99"
+        page.attach_file("item[image]", testing_image)
+        click_on "Upload Item"
 
-      expect(current_path).to eq(admin_items_path)
-      expect(page).to have_content("Onesie")
-      expect(page).to have_content("59.99")
+        expect(current_path).to eq(admin_items_path)
+        expect(page).to have_content("Onesie")
+        expect(page).to have_content("59.99")
+      end
     end
 
     it "I can create an item without an image and it defaults" do
