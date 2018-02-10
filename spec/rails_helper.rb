7688101f035b038f6175d25b2af4f6008a01baf0
maxscores <maxstackhouse@gmail.com>
2018-02-09 18:28:28 -0700

    Add VCR for API Calls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,15 @@ require 'santas_little_helper'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<cloud_name>') {ENV['cloud_name']}
+  config.filter_sensitive_data('<cloudinary_api_key>') {ENV['cloudinary_api_key']}
+  config.filter_sensitive_data('<cloudinary_secret>') {ENV['cloudinary_secret']}
+  config.filter_sensitive_data('<CLOUDINARY_URL>') {ENV['CLOUDINARY_URL']}
+end
+
 RSpec.configure do |config|
   config.include SantasLittleHelper
   config.include FeatureHelper
