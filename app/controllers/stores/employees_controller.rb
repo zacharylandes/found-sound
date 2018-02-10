d5fa989306c40fd87c615e316ed51c3ff1cacd01
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-10 11:11:51 -0700

    updating store role status

--- app/controllers/stores/employees_controller.rb
+++ app/controllers/stores/employees_controller.rb
@@ -1,7 +1,7 @@
 class Stores::EmployeesController < ApplicationController
 
   def update
-    store = Store.find_by(params[:id])
+    store =Store.find_by(slug: params[:store])
     user_store = User.find(params[:user]).user_stores.find_by(store_id: store)
     user_store.update(user_type: params[:change])
     redirect_to "/#{store.slug}/manage"
