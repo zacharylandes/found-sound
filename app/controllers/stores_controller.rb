f2533e81f2b053c4cec4688982d65bf391bd6797
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-09 18:13:03 -0700

    store admin can edit store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,13 +5,16 @@ class StoresController < ApplicationController
   end
 
   def new
+
     @store = Store.new
   end
 
   def create
     store = Store.new(store_params)
     store.users << current_user
+
     if store.save
+      store.user_stores.find_by(user_id: current_user.id).update(user_type:"store_admin")
       redirect_to dashboard_index_path
     else
       flash[:notice] = "Invalid Credentials"
@@ -19,14 +22,30 @@ class StoresController < ApplicationController
     end
   end
 
-  def show
-    @store = Store.find_by(slug: params[:store])
+  def edit
+    @store = current_user.stores.find_by(slug: params[:store])
   end
+
+  def update
+    @store = Store.find(params[:store])
+    @store.update!(update_params)
+    redirect_to "/#{@store.slug}/manage"
+  end
+
+ def show
+    @store = Store.find_by(slug: params[:store])
+ end
 
 private
 
   def store_params
-    params.require(:store).permit(:name, :address, :status)
+    params.require(:store).permit(:name, :address, :slug)
   end
+
+  def update_params
+    params.permit(:name, :address, :slug)
+  end
+
+
 
 end
