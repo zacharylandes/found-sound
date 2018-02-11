c71fb66f42d3127695ee952908c1504947236885
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-11 09:22:32 -0700

    added clear history callback
    This method will find all view objects older than 2 months and delete them

--- app/models/view.rb
+++ app/models/view.rb
@@ -3,4 +3,10 @@ class View < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
+  after_save :clear_history
+
+  def clear_history
+    View.where('created_at < ?', 2.months.ago).destroy_all
+  end
+
 end
