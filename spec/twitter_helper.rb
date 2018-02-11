64f2f7648ed2efc01f4197c154d09f1c59cb22c5
maxscores <maxstackhouse@gmail.com>
2018-02-11 15:11:28 -0700

    Add Twitter Login Omniauth

---
+++ spec/twitter_helper.rb
@@ -0,0 +1,22 @@
+module TwitterHelper
+  def stub_twitter_omniauth
+    # first, set OmniAuth to run in test mode
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: 'twitter',
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Horace",
+          screen_name: "worace"
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
+  end
+end
