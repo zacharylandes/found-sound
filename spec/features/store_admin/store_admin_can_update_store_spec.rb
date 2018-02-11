7ddb7a0b7e263dcd151cbdb630767fb71d5be0e5
maxscores <maxstackhouse@gmail.com>
2018-02-11 15:07:28 -0700

    Write Admin can Auth Store w/ Twitter test

--- spec/features/store_admin/store_admin_can_update_store_spec.rb
+++ spec/features/store_admin/store_admin_can_update_store_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 describe "As a store admin " do
   context "I visit store edit page" do
-    it "I can update store info" do
-      store_admin= create(:user)
-      store = create(:store)
-      user_store = create(:user_store, user: store_admin, store: store, user_type: 1)
-      item1, item2, item3 = create_list(:item, 3, store: store)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(store_admin)
+    before(:each) do
+      @store_admin= create(:user)
+      @store = create(:store)
+      create(:user_store, user: @store_admin, store: @store, user_type: 1)
+      create_list(:item, 3, store: @store)
+    end
 
+    it "I can update store info" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@store_admin)
       visit dashboard_index_path
 
       click_on "My Stores"
       click_on "Manage Store"
-      expect(current_path).to eq ("/#{store.slug}/manage")
+      expect(current_path).to eq ("/#{@store.slug}/manage")
 
       fill_in "name", :with => "A Great Store"
       fill_in "address", :with => "A Great Store"
@@ -22,6 +24,26 @@ describe "As a store admin " do
 
 
 
+    end
+
+    it "can authenticate with twitter" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@store_admin)
+      stub_twitter_omniauth
+      visit dashboard_index_path
+
+      click_on "My Stores"
+      click_on "Manage Store"
+      expect(current_path).to eq ("/#{@store.slug}/manage")
+
+      expect(@store.twitter_token).to be_nil
+      expect(@store.twitter_secret).to be_nil
+
+      click_on "Add Twitter Authorization"
+
+      updated_store = Store.find(@store.id)
+
+      expect(updated_store.twitter_token).to_not be_nil
+      expect(updated_store.twitter_secret).to_not be_nil
     end
   end
 end
