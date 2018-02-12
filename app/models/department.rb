12d174dfe31a51d1a7f91f0e72048374ae7eab01
maxscores <maxstackhouse@gmail.com>
2018-02-12 15:23:18 -0700

    Add Department Model

---
+++ app/models/department.rb
@@ -0,0 +1,11 @@
+class Department < ApplicationRecord
+  has_many :categories
+  has_many :items, through: :categories
+  validates :title, presence: true
+
+  before_save :set_slug
+
+  def set_slug
+    self.slug = title.parameterize
+  end
+end
