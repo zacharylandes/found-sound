6c65faff334a0c51749e8377f0fa8eccb2505378
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-12 16:58:38 -0700

    stripe test

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem 'stripe-ruby-mock', '~> 2.5.2', :require => 'stripe_mock'
 
 gem 'stripe'
 gem 'rails', '~> 5.1.3'
--- spec/features/user/orders/user_can_checkout_from_multiple_stores_spec.rb
+++ spec/features/user/orders/user_can_checkout_from_multiple_stores_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe 'as a registered_user'do
 
-  let!(:user){create(:user)}
   let!(:admin){create(:user)}
   let!(:admin_2){create(:user)}
   let(:department) {create(:department, title: "books")}
@@ -10,112 +9,26 @@ describe 'as a registered_user'do
   let!(:store_1){create(:store, name: "this_store", status:"active", slug: "this_store")}
   let!(:store_2){create(:store, name: "that_store", status:"active", slug: "that_store")}
   let!(:user_store) {create(:user_store, user: admin_2, store: store_2, user_type: 0)}
-  let!(:item_2){create(:item, store: store_2, category_id: thrillers.id, price: 5.00 )}
-  let!(:item_3){create(:item, store: store_2, category_id: thrillers.id, price: 10.00 )}
-  let!(:item_1){create(:item, store: store_1, category_id: thrillers.id, price: 15.00 )}
-  let!(:item_4){create(:item, store: store_1, category_id: thrillers.id, price: 5.00 )}
 
   describe 'when I click checkout' do
+
     it 'shouldnt matter which store I checkout from' do
-    user = create(:user)
+      user = create(:user)
+      item_2 = create(:item, id:2, store: store_2, category_id: thrillers.id, price: 5.00 )
+      item_3 = create(:item, id:3, store: store_2, category_id: thrillers.id, price: 10.00 )
+      item_1 = create(:item, id:1, store: store_1, category_id: thrillers.id, price: 15.00 )
+      item_4 = create(:item, id:4, store: store_1, category_id: thrillers.id, price: 5.00 )
+      order =  create(:order, status: "ordered", user: user)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-        visit departments_department_path(department.slug)
-
-        # add item_2 to cart
-        within(:xpath, '(//div[@class="card-body"])[1]') do
-                click_on "Add to cart"
-        end
-        # add item_3 to cart
-        within(:xpath, '(//div[@class="card-body"])[2]') do
-                click_on "Add to cart"
-        end
-        #add item_1 to cart
-        within(:xpath, '(//div[@class="card-body"])[3]') do
-                click_on "Add to cart"
-        end
-        #add item_4 to cart
-        within(:xpath, '(//div[@class="card-body"])[4]') do
-                click_on "Add to cart"
-        end
-
-        click_on "Cart"
-
-
-        click_on "Checkout"
-
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_2)
-
-      visit dashboard_index_path
-
-      click_on "My Stores"
-      click_on "#{store_2.name}"
-      click_on "Orders"
-
-      within ".order-id" do
-        first(:link).click
-      end
-
+      cart = CartDecorator.new( Cart.new({"1"=>1, "2"=>1, "3"=>1, "4"=>1}))
+      order.add(cart)
+      OrderCreator.new(order).create_store_order
 
       expect(StoreOrder.first.total_price).to eq(15)
+      expect(StoreOrder.last.total_price).to eq(20)
       expect(page).to have_content(15)
 
     end
 
-    it 'shows only orders for a different store' do
-      user = create(:user)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      create(:user_store, user: admin, store: store_1, user_type: 0)
-
-      visit departments_department_path(department.slug)
-
-      # add item_2 to cart
-      within(:xpath, '(//div[@class="card-body"])[1]') do
-              click_on "Add to cart"
-      end
-      # add item_3 to cart
-      within(:xpath, '(//div[@class="card-body"])[2]') do
-              click_on "Add to cart"
-      end
-      # add item_1 to cart
-      within(:xpath, '(//div[@class="card-body"])[3]') do
-              click_on "Add to cart"
-      end
-      # add item_4 to cart
-      within(:xpath, '(//div[@class="card-body"])[4]') do
-              click_on "Add to cart"
-      end
-
-      click_on "Cart"
-
-
-      click_on "Checkout"
-
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
-      visit dashboard_index_path
-
-<<<<<<< HEAD
-      click_on "My Stores"
-      click_on "#{store_1.name}"
-      click_on "Orders"
-=======
-        expect(page).to have_content(item_1.title)
-        expect(page).to_not have_content(item_2.title)
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_2)
->>>>>>> refactor
-
-      within ".order-id" do
-        first(:link).click
-      end
-
-      expect(page).to have_content(20)
-      expect(StoreOrder.last.total_price).to eq(20)
-    end
   end
 end
