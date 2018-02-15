7c29b7b9f483a176f20c5f91f496ff951cb5ad2a
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-15 16:55:03 -0700

    Moved API call out of controller into a servicer

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -4,12 +4,8 @@ class ShippingController < ApplicationController
   end
 
   def show
-    @response = Faraday.post("https://api.easypost.com/v2/shipments?shipment[to_address][name]=#{current_user.full_name}&shipment[to_address][street1]=#{shipping_params[:street]}&shipment[to_address][city]=#{shipping_params[:city]}&shipment[to_address][state]=#{shipping_params[:state]}&shipment[to_address][zip]=#{shipping_params[:zip]}&shipment[to_address][country]=US&shipment[to_address][phone]=#{shipping_params[:phone]}&shipment[to_address][email]=#{current_user.email}&shipment[from_address][name]=Found Sound&shipment[from_address][street1]=1331 17th st&shipment[from_address][city]=Denver&shipment[from_address][state]=CO&shipment[from_address][zip]=80219&shipment[from_address][country]=US&shipment[from_address][phone]=4153334445&shipment[from_address][email]=support@foundsound.com&shipment[parcel][length]=20.2&shipment[parcel][width]=10.9&shipment[parcel][height]=5&shipment[parcel][weight]=65.9&api_key=#{ENV['easypost_key']}")
-    shipment = JSON.parse(@response.body, symbolize_names: true)[:rates]
-    # binding.pry
-    @rates = shipment.map do |rate|
-      Rate.new(rate)
-    end
+    easypost = EasypostService.new(shipping_params, current_user)
+    @rates = easypost.rates
   end
 
   private
--- app/services/easypost_service.rb
+++ app/services/easypost_service.rb
@@ -1,15 +1,16 @@
 class EasypostService
 
-  def initialize
-    # @conn = Faraday.new(url: "https://api.easypost.com/v2") do |faraday|
-    #   faraday.headers['api_key'] = "ENV['easypost_key']"
-    #   faraday.adapter Faraday.default_adapter
-    # end
+  attr_reader :response, :shipment
+
+  def initialize(shipping_params, user)
+    @response = Faraday.post("https://api.easypost.com/v2/shipments?shipment[to_address][name]=#{user.full_name}&shipment[to_address][street1]=#{shipping_params[:street]}&shipment[to_address][city]=#{shipping_params[:city]}&shipment[to_address][state]=#{shipping_params[:state]}&shipment[to_address][zip]=#{shipping_params[:zip]}&shipment[to_address][country]=US&shipment[to_address][phone]=#{shipping_params[:phone]}&shipment[to_address][email]=#{user.email}&shipment[from_address][name]=Found Sound&shipment[from_address][street1]=1331 17th st&shipment[from_address][city]=Denver&shipment[from_address][state]=CO&shipment[from_address][zip]=80219&shipment[from_address][country]=US&shipment[from_address][phone]=4153334445&shipment[from_address][email]=support@foundsound.com&shipment[parcel][length]=20.2&shipment[parcel][width]=10.9&shipment[parcel][height]=5&shipment[parcel][weight]=65.9&api_key=#{ENV['easypost_key']}")
+    @shipment = JSON.parse(@response.body, symbolize_names: true)[:rates]
   end
 
-  def addresses
-    response = Faraday.post("https://api.easypost.com/v2/shipments?shipment[to_address][name]=Dr. Steve Brule&shipment[to_address][street1]=179 N Harbor Dr&shipment[to_address][city]=Redondo Beach&shipment[to_address][state]=CA&shipment[to_address][zip]=90277&shipment[to_address][country]=US&shipment[to_address][phone]=8573875756&shipment[to_address][email]=dr_steve_brule@gmail.com&shipment[from_address][name]=EasyPost&shipment[from_address][street1]=417 Montgomery st&shipment[from_address][street2]=5th Floor&shipment[from_address][city]=San Francisco&shipment[from_address][state]=CA&shipment[from_address][zip]=94104&shipment[from_address][country]=US&shipment[from_address][phone]=4153334445&shipment[from_address][email]=support@easypost.com&shipment[parcel][length]=20.2&shipment[parcel][width]=10.9&shipment[parcel][height]=5&shipment[parcel][weight]=65.9&api_key=ENV['easypost_key']")
-    b = JSON.parse(response.body)
+  def rates
+    @shipment.map do |rate|
+      Rate.new(rate)
+    end
   end
 
 end
