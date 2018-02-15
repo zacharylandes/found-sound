58f8c20ee17f9e67be62a49ae1f89ae3fc2bca3b
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-15 14:40:09 -0700

    modified tests for checking out with stripe to work with API calls

--- spec/features/user/items/user_can_place_orders_spec.rb
+++ spec/features/user/items/user_can_place_orders_spec.rb
@@ -3,35 +3,35 @@ require 'rails_helper'
 RSpec.feature "User can place an order" do
 
   it "and see the message 'order was successfully placed'" do
-      VCR.use_cassette("stripe") do
+    VCR.use_cassette("stripe") do
 
-    user = create(:user, first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address")
+      user = create(:user, first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address")
 
-    item1, item2, item3 = create_list(:item, 3)
+      item1, item2, item3 = create_list(:item, 3)
 
-    visit store_path(item1.store.slug)
+      visit store_path(item1.store.slug)
 
-    click_on "Add to cart"
+      click_on "Add to cart"
 
-    visit cart_path
+      visit cart_path
 
-    within(".checkout") do
-      click_on("Login")
+      within(".checkout") do
+        click_on("Login")
+      end
+
+      fill_in "session[email]", with: "testerson@testmail.com"
+      fill_in "session[password]", with: "testing"
+
+      within(".action") do
+        click_on("Login")
+      end
+
+      click_on "Cart"
+
+      expect(page).to have_link("Checkout")
+
+      click_on "Checkout"
+      expect(current_path).to eq('/shipping')
     end
-
-    fill_in "session[email]", with: "testerson@testmail.com"
-    fill_in "session[password]", with: "testing"
-
-    within(".action") do
-      click_on("Login")
-    end
-
-    click_on "Cart"
-
-    expect(page).to have_link("Checkout")
-
-    click_on "Checkout"
-    expect(current_path).to eq('/orders/new')
-  end
   end
 end
--- spec/features/user/payments/user_can_checkout_with_stripe_spec.rb
+++ spec/features/user/payments/user_can_checkout_with_stripe_spec.rb
@@ -2,33 +2,45 @@ require 'rails_helper'
 
 RSpec.feature "User can checkout with stripe" do
   it "and see the message 'order was successfully placed'" do
+    VCR.use_cassette("shipment response") do
+      user = create(:user, first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address")
+      item1, item2, item3 = create_list(:item, 3)
+      visit store_path(item1.store.slug)
 
-    user = create(:user, first_name: "Tester", last_name: "McTesty", email: "testerson@testmail.com", password: "testing", address: "dummy address")
+      click_on "Add to cart"
 
-    item1, item2, item3 = create_list(:item, 3)
+      visit cart_path
 
-    visit store_path(item1.store.slug)
+      within(".checkout") do
+        click_on("Login")
+      end
 
-    click_on "Add to cart"
+      fill_in "session[email]", with: "testerson@testmail.com"
+      fill_in "session[password]", with: "testing"
 
-    visit cart_path
+      within(".action") do
+        click_on("Login")
+      end
 
-    within(".checkout") do
-      click_on("Login")
+      click_on "Cart"
+      click_on 'Checkout'
+
+      expect(current_path).to eq(shipping_path)
+
+      fill_in "shipping[street]", with: "944 s quitman st"
+      fill_in "shipping[city]", with: "Denver"
+      fill_in "shipping[state]", with: "CO"
+      fill_in "shipping[zip]", with: "80219"
+      fill_in "shipping[country]", with: "US"
+      fill_in "shipping[phone]", with: "5555555555"
+      click_on "Submit"
+
+      within(first(".rate")) do
+        click_on "Select"
+      end
+
+      expect(current_path).to eq(new_order_path)
+      expect(page).to have_content("32.98")
     end
-
-    fill_in "session[email]", with: "testerson@testmail.com"
-    fill_in "session[password]", with: "testing"
-
-    within(".action") do
-      click_on("Login")
-    end
-
-    click_on "Cart"
-    #
-    click_on 'Checkout'
-
-    expect(current_path).to eq(new_order_path)
-    expect(page).to have_content("$10.00")
   end
 end
