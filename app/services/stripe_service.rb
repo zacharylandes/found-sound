c514dabfc00e1c436d42964959f7a48edaa7f913
ZacharyLandes <zach.g.landes@gmail.com>
2018-02-15 15:24:50 -0700

    pull master

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,24 +1,13 @@
 class StripeService
 
   def stripe_customer(email,token, order)
-
-    if StripeCustomer.find_by(email: email)
-      customer = StripeCustomer.find_by(email: email)
-      charge_customer(order, customer)
-    else
-
-    customer = Stripe::Customer.create(
-      :email => email,
-      :source  => token
-    )
-    customer = StripeCustomer.create!(email: email, token:token, stripe_id: customer.id)
-    end
-  end
-
-    def charge_customer(order,customer)
+      customer = Stripe::Customer.create(
+        :email => email,
+        :source  => token
+      )
       charge = Stripe::Charge.create(
-        :customer    => customer.stripe_id,
-        :amount      => (order.total_price * 100).to_i,
+        :customer    => customer.id,
+        :amount      => order.total_price.to_i,
         :description => 'Rails Stripe customer',
         :currency    => 'usd'
       )
