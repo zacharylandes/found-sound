7c17cdfe31b7b1931ff83e4dc54790a0fc4f17a7
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-14 20:50:20 -0700

    added routes for shipping functionality

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/shipping', to: 'shipping#new', as: 'shipping'
+  post '/shipping/show', to: 'shipping#show'
+
   resources :stores, only:[:index, :new, :create]
   get '/:store/items/new', to: "items#new", as: "new_store_item"
   post '/:store/items', to: "items#create", as: "store_items"
@@ -72,5 +75,6 @@ Rails.application.routes.draw do
 
   get "/settings/developer", to: 'developer#show', as: 'developer'
   post "/settings/developer", to: 'developer#create'
+
 
 end
